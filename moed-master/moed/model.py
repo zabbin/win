c896256de14a9280bfe49d65feee97c204f4548f
zabbin <36194227+zabbin@users.noreply.github.com>
2020-01-21 07:47:41 +0300

    new

---
+++ moed-master/moed/model.py
@@ -0,0 +1,95 @@
+import math
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+
+class Model:
+
+    @staticmethod
+    def linear(range_, a=1, b=0):
+        return Sequence.from_func(range_, lambda x: a*x + b)
+
+    @staticmethod
+    def exponential(range_, alpha=1, beta=1):
+        return Sequence.from_func(range_, lambda x: beta * math.e ** (alpha * x))
+    
+    @staticmethod
+    def harmonic(range_, amp=1, freq=1, phase=0):
+        return Sequence.from_func(range_, lambda x: math.sin(2 * math.pi * x * freq + phase) * amp)
+
+
+class Sequence:
+
+    def __init__(self):
+        self._seq = OrderedDict()
+    
+    def plot(self):
+        plt.figure(figsize=(15, 5))
+        plt.plot(self.x, self.y)
+        plt.show()
+    
+    @property
+    def x(self):
+        return list(self._seq.keys())
+    
+    @property
+    def y(self):
+        return list(self._seq.values())
+
+    @staticmethod
+    def from_func(x_range, f):
+        res = Sequence()
+        for x in x_range:
+            res._seq[x] = f(x)
+        return res
+
+    @staticmethod
+    def from_dict(ordered_dict):
+        if isinstance(ordered_dict, OrderedDict):
+            res = Sequence()
+            res._seq = ordered_dict.copy()
+            return res
+        raise ValueError("Dictionary must be OrderedDict")
+    
+    def __len__(self):
+        return len(self._seq)
+
+    def __add__(self, other):
+        res = Sequence()
+        keys = list(set(self.x).union(other.x))
+        keys.sort()
+        for x in keys:
+            res._seq[x] = 0
+            if x in self.x:
+                res._seq[x] += self._seq[x]
+            if x in other.x:
+                res._seq[x] += other._seq[x]
+        return res
+    
+    def __mul__(self, other):
+        res = Sequence()
+        keys = list(set(self.x).union(other.x))
+        keys.sort()
+        for x in keys:
+            res._seq[x] = 1
+            if x in self.x:
+                res._seq[x] *= self._seq[x]
+            if x in other.x:
+                res._seq[x] *= other._seq[x]
+        return res
+    
+    def __sub__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Sequence().from_func(self.x, lambda x: self._seq[x] - other)
+        if isinstance(other, Sequence):
+            return Sequence.from_func(self.x, lambda x: self._seq[x] - other._seq[x])
+        else:
+            raise ValueError("Operand of this types is not supported")
+    
+    def __truediv__(self, other):
+        return Sequence.from_func(self.x, lambda x: self._seq[x] / other)
+    
+    def hamming(self, alpha=0.46):
+        size = len(self)
+        y = self.y
+        return Sequence.from_func(range(size), lambda x: y[x] * (alpha  - (1 - alpha) * math.cos(2 * math.pi * x / size)))
